653d25ad94de968584dad9da4c705e41c7d6f4bd
VerbusComunicacao <ferreira@verbuscomunicacao.com.br>
2025-01-07 14:36:48 -0300

    melhora retorno de erro wordpress lib

--- src/lib/wordpress.ts
+++ src/lib/wordpress.ts
@@ -1,14 +1,16 @@
-import { Category } from "@/types/category"
-
 const WP_URL = process.env.NEXT_PUBLIC_WORDPRESS_URL || 'http://localhost/wordpress'
 
 export async function fetchAPI(query: string) {
-  const res = await fetch(`${WP_URL}/graphql`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query }),
-  })
-  
-  const json = await res.json()
-  return json.data
+  try {
+    const res = await fetch(`${WP_URL}/graphql`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query }),
+    })
+    
+    const json = await res.json()
+    return json.data
+  } catch (error) {
+    throw new Error('Erro: Verifique se o WordPress está rodando')
+  }
 }
